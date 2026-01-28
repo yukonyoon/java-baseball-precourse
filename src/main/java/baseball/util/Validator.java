307368daf848236da84d2e59f0704bd1290c81ff
yukon.yoon <yukon.yoon@kakaocorp.com>
2026-01-28 17:20:35 +0900

    feat: 숫자의 길이가 3이 넘어가는 입력에 대한 예외 처리 추가

--- src/main/java/baseball/util/Validator.java
+++ src/main/java/baseball/util/Validator.java
@@ -2,9 +2,12 @@ package baseball.util;
 
 public class Validator {
 
+    private static int THRESHOLD_LENGTH = 3;
+
     public boolean validateInput(String input) {
         try{
             checkNumberType(input);
+            checkNumberLength(input);
         } catch (IllegalArgumentException e) {
             return false;
         }
@@ -16,6 +19,13 @@ public class Validator {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             System.out.println("[ERROR] " + "숫자 형식이 압니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkNumberLength(String input) {
+        if (input.length() != THRESHOLD_LENGTH) {
+            System.out.println("[ERROR] " + "입력 길이가 3이 아닙니다.");
             throw new IllegalArgumentException();
         }
     }
--- src/test/java/baseball/util/ValidatorTest.java
+++ src/test/java/baseball/util/ValidatorTest.java
@@ -17,9 +17,22 @@ public class ValidatorTest {
 
     @Test
     @DisplayName("숫자 형식이 아닌 입력이 들어오면 예외를 발생시킨다.")
-    void 금액_입력_예외_테스트_1() throws Exception {
+    void 숫자_입력_예외_테스트_1() throws Exception {
         // given
         String input = "aaa";
+
+        // when
+        boolean result = validator.validateInput(input);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("입력의 길이가 3 이상인 경우 예외를 발생시킨다.")
+    void 숫자_입력_예외_테스트_2() throws Exception {
+        // given
+        String input = "1234";
 
         // when
         boolean result = validator.validateInput(input);
